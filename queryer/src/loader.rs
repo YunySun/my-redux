842850cc6e958e3ddd916929318b17f34b14a15d
李昶 <48754120+YunySun@users.noreply.github.com>
2022-10-26 17:28:55 +0800

    sql

---
+++ queryer/src/loader.rs
@@ -0,0 +1,51 @@
+/*
+ * @Description:
+ * @Author: 李昶
+ * @Date: 2022-10-26 15:18:13
+ * @LastEditors: 李昶
+ * @LastEditTime: 2022-10-26 15:18:17
+ * @Profile: 一个比较废柴的前端开发
+ */
+
+use crate::DataSet;
+use anyhow::Result;
+use polars::prelude::*;
+use std::io::Cursor;
+
+pub trait Load {
+    type Error;
+    fn load(self) -> Result<DataSet, Self::Error>;
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum Loader {
+    Csv(CsvLoader),
+}
+
+#[derive(Default, Debug)]
+pub struct CsvLoader(pub(crate) String);
+
+impl Loader {
+    pub fn load(self) -> Result<DataSet> {
+        match self {
+            Loader::Csv(csv) => csv.load(),
+        }
+    }
+}
+
+pub fn detect_content(data: String) -> Loader {
+    // TODO: 内容检测
+    Loader::Csv(CsvLoader(data))
+}
+
+impl Load for CsvLoader {
+    type Error = anyhow::Error;
+
+    fn load(self) -> Result<DataSet, Self::Error> {
+        let df = CsvReader::new(Cursor::new(self.0))
+            .infer_schema(Some(16))
+            .finish()?;
+        Ok(DataSet(df))
+    }
+}
